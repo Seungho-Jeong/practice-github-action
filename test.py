27b8a09525ca1efb666ca2ff6229286e4336d3d4
Seungho Jeong <sh007.jeong@gmail.com>
2022-03-23 15:32:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+import requests
+
+from bs4 import BeutifulSoup
+
+URL = 'https://ridibooks.com/category/new-releases/2200'
+
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+bookservices = soup.select('.title_text')
+
+for number, book in enumerate(bookservices, 1):
+    print(number, book.text.strip())
